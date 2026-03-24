7638784d0509dd1754b8701ffcdd61ac0bca00e5
matematik-isto <ragnarok_wt@rocketmail.com>
2026-03-24 03:00:31 -0600

    checkbox_millas
    Se incluye funcionalidad para filtrar qué parte de la información se muestra mediante un checkbox. sobre autos con más de 100 000 millas

--- app.py
+++ app.py
@@ -24,41 +24,56 @@ def main() -> None:
     """
     # Leer los datos del archivo.csv
     car_data = pd.read_csv('data/vehicles_us.csv')
-    st.header('Lanzar una moneda')
-    st.write('Esta aplicación aún no es funcional. En construcción.')
-    # Crear un botón en la aplicación Streamlit
-    hist_button = st.button('Construir histograma')
-    scatter_button = st.button('Construir gráifca de dispersión')
-    
+    st.header('¡Autos! Autos!!')
+    st.write('Se tiene un conjunto de datos sobre ventas de autos, el precio junto con características del vehículo.')
+    st.write('Exploremos características interesantes.')
+    # st state
+    if "hist_button" not in st.session_state:
+        st.session_state.hist_button = False
+    if "scatter_button" not in st.session_state:
+        st.session_state.scatter_button = False
+    if "miles_chechbox" not in st.session_state:
+        st.session_state.miles_checkbox = False
+    # Botones
+    if st.button("Construir histograma"):
+        st.session_state.hist_button = True
+
+    if st.button("Construir gráfica de dispersión"):
+        st.session_state.scatter_button = True
+
+    if st.checkbox("¿mostrar autos con más de 100 000 millas?"):
+        st.session_state.miles_checkbox = True
+
+    col1, col2 = st.columns(2)
+
+    with col1:  
     # Lógica a ejecutar cuando se hace clic en el botón
-    if hist_button:
-        # Escribir un mensaje en la aplicación
-        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        if st.session_state.hist_button:
+            # Escribir un mensaje en la aplicación
+            st.write('Histograma de datos de odómetro')
 
-        # Crear un histograma utilizando plotly.graph_objects
-        # Se crea una figura vacía y luego se añade un rastro de histograma
-        fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+            # Crear un histograma utilizando plotly.graph_objects
+            # Se crea una figura vacía y luego se añade un rastro de histograma
+            if st.session_state.miles_checkbox:
+                fig1 = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+            else:
+                fig1 = go.Figure(data=[go.Histogram(x=car_data[car_data['odometer'] < 100000.0]['odometer'])])
+            fig1.update_layout(title_text='Distribución del Odómetro')
+            st.plotly_chart(fig1, use_container_width=True)
+#            st.session_state.hist_button = False
 
-        # Opcional: Puedes añadir un título al gráfico si lo deseas
-        fig.update_layout(title_text='Distribución del Odómetro')
-
-        # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
-        # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
-        st.plotly_chart(fig, use_container_width=True)
-     
-    # Lógica a ejecutar cuando se hace clic en el botón
-    if scatter_button:
-        # Escribir un mensaje en la aplicación
-        st.write('Creación de una gráfica de dispersión para comparar kilometraje contra precio')
-        # Crear un scatter plot utilizando plotly.graph_objects
-        # Se crea una figura vacía y luego se añade un rastro de scatter
-        fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
-
-        # Opcional: Puedes añadir un título al gráfico si lo deseas
-        fig.update_layout(title_text='Relación entre Odómetro y Precio')
-
-        # Mostrar el gráfico Plotly
-        fig.show()
+    with col2:      
+        # Lógica a ejecutar cuando se hace clic en el botón
+        if st.session_state.scatter_button:
+            # Escribir un mensaje en la aplicación
+            st.write('Gráfica de dispersión kilometraje contra precio')
+            # Crear un scatter plot utilizando plotly.graph_objects
+            if st.session_state.miles_checkbox:
+                fig2 = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+            else:
+                fig2 = go.Figure(data=[go.Scatter(x=car_data[car_data['odometer'] < 100000.0]['odometer'], y=car_data[car_data['odometer'] < 100000.0]['price'], mode='markers')])
+            fig2.update_layout(title_text='Relación entre Odómetro y Precio')
+            st.plotly_chart(fig2, use_container_width=True)
 
 # Punto de entrada del script
 if __name__ == "__main__":
